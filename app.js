5ce65fcdff67a07f6a316caa34d787c9d638d09f
holera <holera91@gmail.com>
2022-08-24 23:36:30 +0300

    отримуємо курси всіх валют в data

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
-//перевірка курсів Монобанк
-const axios = require('axios').default;axios.get('https://api.monobank.ua/bank/currency')
-.then(function (response) {
-  // handle success
-  console.log(response);
-})
-.catch(function (error) {
-  // handle error
-  console.log(error);
-})
-.then(function () {
-  // always executed
-});
+
+
+
+
+//отримуємо курси від Монобанк
+const axios = require('axios').default;
+const monoUrl = 'https://api.monobank.ua/bank/currency';
+const curCur = (response) => {
+const {data} = response;
+   };
+axios.get(monoUrl).then(curCur);
+
+
+    
+
+
+//витягуємо з масиву курс USD, EUR,POL
+/*
+        for (let i = 0; i < curCur.length; i++)
+        {  
+        if((curCur[i].currencyCodeA == 840) && (curCur[i].currencyCodeB == 980))
+        {
+        const usdBuy = curCur[i].rateBuy;
+        const usdSell = curCur[i].rateSell;    
+        console.log(`USD покупка${usdBuy}`);
+        console.log(`USD продаж${usdSell}`);
+        }
+        if((curCur[i].currencyCodeA == 978) && (curCur[i].currencyCodeB == 980))
+        {
+        const eurBuy = curCur[i].rateBuy;
+        const eurSell = curCur[i].rateSell;    
+        console.log(`EUR покупка${eurBuy}`);
+        console.log(`EUR продаж${eurSell}`);
+        }
+        if((curCur[i].currencyCodeA == 985) && (curCur[i].currencyCodeB == 980))
+        {
+        const polZl = curCur[i].rateCross;  
+        console.log(`Курс Злотий${polZl}`);
+    
+        }
+        else
+        {
+        console.log("нема нічьо")
+        }
+    
+        }
+        */
+     
+
+    
