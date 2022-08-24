8fbeb171fa4497f76061cf655906127cf78fc33b
holera <holera91@gmail.com>
2022-08-24 20:53:03 +0300

    add GET request on mono API

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Брий зад")
+//перевірка курсів Монобанк
+const axios = require('axios').default;axios.get('https://api.monobank.ua/bank/currency')
+.then(function (response) {
+  // handle success
+  console.log(response);
+})
+.catch(function (error) {
+  // handle error
+  console.log(error);
+})
+.then(function () {
+  // always executed
+});
