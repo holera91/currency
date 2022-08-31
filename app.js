6516bcdee0f49bc5799a8c39986052d2230ccc71
holera <holera91@gmail.com>
2022-08-31 23:54:07 +0300

    фікси

--- app.js
+++ app.js
@@ -10,45 +10,39 @@ let usd;
 function curCur(response) {
     const { data } = response;
 
-    console.log(data);
+    //console.log(data);
 }
-    axios.get(privatUrl).then(curCur);
+axios.get(monoUrl).then(curCur);
 
 
-    
+
 
 
 //витягуємо з масиву курс USD, EUR,POL
 /*
-        for (let i = 0; i < curCur.length; i++)
-        {  
-        if((curCur[i].currencyCodeA == 840) && (curCur[i].currencyCodeB == 980))
-        {
+for (let i = 0; i < curCur.length; i++) {
+    if ((curCur[i].currencyCodeA == 840) && (curCur[i].currencyCodeB == 980)) {
         const usdBuy = curCur[i].rateBuy;
-        const usdSell = curCur[i].rateSell;    
+        const usdSell = curCur[i].rateSell;
         console.log(`USD покупка${usdBuy}`);
         console.log(`USD продаж${usdSell}`);
-        }
-        if((curCur[i].currencyCodeA == 978) && (curCur[i].currencyCodeB == 980))
-        {
+    }
+    if ((curCur[i].currencyCodeA == 978) && (curCur[i].currencyCodeB == 980)) {
         const eurBuy = curCur[i].rateBuy;
-        const eurSell = curCur[i].rateSell;    
+        const eurSell = curCur[i].rateSell;
         console.log(`EUR покупка${eurBuy}`);
         console.log(`EUR продаж${eurSell}`);
-        }
-        if((curCur[i].currencyCodeA == 985) && (curCur[i].currencyCodeB == 980))
-        {
-        const polZl = curCur[i].rateCross;  
+    }
+    if ((curCur[i].currencyCodeA == 985) && (curCur[i].currencyCodeB == 980)) {
+        const polZl = curCur[i].rateCross;
         console.log(`Курс Злотий${polZl}`);
-    
-        }
-        else
-        {
-        console.log("нема нічьо")
-        }
-    
-        }
-        */
-     
 
-    
+    }
+    else {
+        console.log("нема нічьо")
+    }
+
+}
+
+*/
+
